cf6631a804b9a9530b26fc8aa31100865c40557e
Farhod Akramov <akramovfrank@gmail.com>
2023-04-16 21:39:38 -0600

    forgot to push

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -2,7 +2,13 @@ import React from "react";
 import "../styles/Page404.css";
 
 function Page404() {
-  return <div className="Page404">Sorry this doesn't work just yet</div>;
+  return (
+    <div className="Page404">
+      <div id="gifHolder"></div>
+      <div id="text"></div>
+      <div id="button"></div>
+    </div>
+  );
 }
 
 export default Page404;
