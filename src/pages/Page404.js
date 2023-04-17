cab1fbc94d31a093944b950d2a4d7d4c822aba03
Farhod Akramov <akramovfrank@gmail.com>
2023-04-16 22:36:43 -0600

    added a fully functional 404 page and made it mobile responsive

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -1,12 +1,23 @@
 import React from "react";
 import "../styles/Page404.css";
+import page404gif from "../assets/Page404.gif";
+import { Link } from "react-router-dom";
 
 function Page404() {
   return (
     <div className="Page404">
-      <div id="gifHolder"></div>
-      <div id="text"></div>
-      <div id="button"></div>
+      <div id="content">
+        <div
+          id="gifHolder"
+          style={{ backgroundImage: `url(${page404gif})` }}
+        ></div>
+        <div id="text">
+          {`Oopsies, looks like the page you are looking for doesn't exist, try exploring other parts of this website ◡̈`}
+        </div>
+      </div>
+      <Link id="button" to="/">
+        Go Home
+      </Link>
     </div>
   );
 }
