c68882459134972c6c34e68e66cd3127d9f99d53
Farhod Akramov <akramovfrank@gmail.com>
2023-04-16 18:04:36 -0600

    added project screenshots, time to add descriptions and event handlers

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -3,7 +3,7 @@ import React from "react";
 function ProjectItem({ image, name, description }) {
   return (
     <div className="projectItem">
-      <div style={{ backgroundImage: `url(${image})` }}></div>
+      <div id="imgWrap" style={{ backgroundImage: `url(${image})` }}></div>
       <h1>{name}</h1>
       <p>{description}</p>
     </div>
--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,39 +1,58 @@
-import pic from "../assets/projectPlaceholder.jpeg";
+import readmeGen from "../assets/readmeGenCli.png";
+import quizApp from "../assets/quiz.png";
+import noteTaker from "../assets/noteTaker.png";
+import weatherDash from "../assets/weatherDash.png";
+import digitalLibrarian from "../assets/digitalLibrarian.png";
+import mercuryPos from "../assets/mercuryPos.png";
+import workDayScheduler from "../assets/workDayScheduler.png";
+import passwordGen from "../assets/passwordGen.png";
 
 export const ProjectList = [
   {
-    name: "First Project",
-    image: pic,
+    name: "Digital Librarian",
+    image: digitalLibrarian,
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam, esse!",
   },
   {
-    name: "Second Project",
-    image: pic,
+    name: "Mercury POS",
+    image: mercuryPos,
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam, esse!",
   },
   {
-    name: "Third Project",
-    image: pic,
+    name: "CLI Readme Generator",
+    image: readmeGen,
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam, esse!",
   },
   {
-    name: "Fourth Project",
-    image: pic,
+    name: "Quiz Application",
+    image: quizApp,
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam, esse!",
   },
   {
-    name: "Fifth Project",
-    image: pic,
+    name: "Note Taker",
+    image: noteTaker,
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam, esse!",
   },
   {
-    name: "Sixth Project",
-    image: pic,
+    name: "Weather Dashboard",
+    image: weatherDash,
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam, esse!",
+  },
+  {
+    name: "Work-Day Scheduler",
+    image: workDayScheduler,
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam, esse!",
+  },
+  {
+    name: "Password Generator",
+    image: passwordGen,
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam, esse!",
   },
