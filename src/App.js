b8a38abcfabdfeb533e8b653de61167b8e48c50b
Farhod Akramov <akramovfrank@gmail.com>
2023-04-18 10:22:08 -0600

    hash router

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Page404 from "./pages/Page404";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
